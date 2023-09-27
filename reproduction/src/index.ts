317667b62af38aa3908dd34a3e462222e0dfac75
Eric Allam <eallam@icloud.com>
2023-09-27 10:48:21 +0100

    Try the other way around (package uses Zod 3.22.2 and usage using 3.21.4)

--- reproduction/src/index.ts
+++ reproduction/src/index.ts
@@ -3,16 +3,16 @@ import {
   safeParsePayload,
   safeParsePayloadInfer,
   safeParsePayloadVariant,
-} from "safe-parse-payload"; // this package uses zod@3.21.4
+} from "safe-parse-payload"; // this package uses zod@3.22.2
 
-// This schema is 3.22.2
+// This schema is 3.21.4
 const schema = z.object({ id: z.string() });
 
 // safeParsePayload uses z.output<T>
-const payload = await safeParsePayload(schema, { id: "123" });
+const payload = safeParsePayload(schema, { id: "123" });
 
 // safeParsePayloadInfer uses z.infer<T>
-const payload2 = await safeParsePayloadInfer(schema, { id: "123" });
+const payload2 = safeParsePayloadInfer(schema, { id: "123" });
 
 // safeParsePayloadVariant uses z.Schema<T> and will cause the "Type instantiation is excessively deep and possibly infinite" error
 const payload3 = safeParsePayloadVariant(schema, { id: "123" });
